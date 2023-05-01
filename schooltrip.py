a9524a84a980c368a3185760a66f0f83c3f8d161
pokemonmaster10706 <112710126+pokemonmaster10706@users.noreply.github.com>
2023-05-01 21:06:50 +0400

    Update schooltrip.py

--- schooltrip.py
+++ schooltrip.py
@@ -1,6 +1,15 @@
 coach_cost = 550
 entryticket_cost = 30
-student_count = int(input('enter the number of student who wish to go to the school trip : '))
+while True:
+    try:
+        student_count = int(input('enter the number of student who wish to go to the school trip : '))
+        if student_count > 45:
+            print('max students allowed is 45')
+            continue
+    except:
+        print('please enter a valid integer value')
+
+
 total_cost = (entryticket_cost*student_count)-(student_count//10)+coach_cost
 costper_student = (total_cost/student_count)+1
 print(costper_student)
