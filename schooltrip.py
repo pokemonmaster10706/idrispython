2e7e36685dcb1c660efeb158e98dc73122f5cb87
pokemonmaster10706 <112710126+pokemonmaster10706@users.noreply.github.com>
2023-05-02 22:37:18 +0400

    update schooltrip.py

--- schooltrip.py
+++ schooltrip.py
@@ -1,6 +1,15 @@
 coach_cost = 550
 entryticket_cost = 30
 student_dict = {}
+accepted = []
+rejected = []
+run_time = 1
+
+def cost(num):
+    total_cost = (entryticket_cost*num)-(num//10)+coach_cost
+    costper_student = (total_cost/num)+1
+    return costper_student
+
 
 while True:
     try:
@@ -10,22 +19,38 @@ while True:
             continue
     except:
         print('please enter a valid integer value')
-
-for i in range(student_count):
+        continue
+costper_student = cost(student_count)
+print('each student must pay a total of ',costper_student)
+while True:
     name = input('enter student '+str(i+1)+' name:  ')
     if name in student_dict:
         print(name,'has already been entered, please enter next student')
         continue
     else:
+        run_time += 1
         pay_status=input('has this student payed for the trip(y/n)?   \n\n')
         if pay_status == 'y':
             pay = 'paid'
+            accepted.append(name)
         else:
             pay = 'pending'
+            rejected.append(name)
         student_dict[name]=pay
 
+for k,v in student_dict:
+    print('student name:',k,'\npayement status:',v,'\n')
 
 
-total_cost = (entryticket_cost*student_count)-(student_count//10)+coach_cost
-costper_student = (total_cost/student_count)+1
-print(costper_student)
+
+
+print('the students who have paid and will be going to the trip are : ')
+for i in accepted:
+    print(i)
+else:
+    print('-'*30)
+print('the students who havent paid yet are :')
+for i in rejected:
+    print(i)
+else:
+    print('-'*30)
