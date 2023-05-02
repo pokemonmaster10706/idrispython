7da9807357839723403c50688975c26e76ba6bb6
pokemonmaster10706 <112710126+pokemonmaster10706@users.noreply.github.com>
2023-05-02 19:17:15 +0400

    Update login.py

---
+++ login.py
@@ -0,0 +1,13 @@
+user_count = input('enter the number of users who will be loging in this time?\n\n')
+
+check = 1
+while True:
+    username = input('user '+check+' enter your username :  ')
+    if username in details:
+        print('the user name',username,'is already in use, please enter a different one.')
+        continue
+    password = input('enter a password for '+username+' \npassword must contain more than 8 characters,a capital and a lowercase alphabet, a number, and a special character :')
+
+
+
+    
